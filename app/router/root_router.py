ae588fc616b8dadc45c1b551d7616a40e131ba6a
Tanvi Adhikari <adhikaritanvi22@gmail.com>
2025-06-21 13:57:51 +0530

    this is a trial

--- app/router/root_router.py
+++ app/router/root_router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from router.inquiry_form_router import router as inquiry_form_router
-from router.consultation_form_router import router as consultation_form_router
-from router.auth_router import router as auth_router
-from router.login_router import router as login_router
+from app.router.inquiry_form_router import router as inquiry_form_router
+from app.router.consultation_form_router import router as consultation_form_router
+from app.router.auth_router import router as auth_router
+from app.router.login_router import router as login_router
 
 def root_router():
     api_router = APIRouter(prefix="/api")
